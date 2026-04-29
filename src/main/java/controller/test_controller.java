3970befbcfdc660d0cddedc3a106f0edeadf075a
Twinedline <Twinedline@gmail.com>
2026-04-29 15:03:45 +0100

    testing the api

---
+++ src/main/java/controller/test_controller.java
@@ -0,0 +1,13 @@
+package com.keelan.hospitalapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test_controller {
+
+    @GetMapping("/test")
+    public String test() {
+        return "YIPPIE!";
+    }
+}
